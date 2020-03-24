e7dd66718170ae8963ee7b11376500b2cc14cffd
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-24 14:48:31 +0530

    updates

--- main.py
+++ main.py
@@ -28,3 +28,16 @@ class MainWindow(QMainWindow):
         self.viewfinder = QCameraViewfinder()
         self.viewfinder.show()
         self.setCentralWidget(self.viewfinder)
+
+        # Select the default camera.
+        self.select_camera(0)
+
+
+        camera_toolbar = QToolBar("Camera")
+        camera_toolbar.setIconSize(QSize(14, 14))
+        self.addToolBar(camera_toolbar)
+
+        photo_action = QAction(QIcon(os.path.join('images', 'camera-black.png')), "Take photo...", self)
+        photo_action.setStatusTip("Take photo of current view")
+        photo_action.triggered.connect(self.take_photo)
+        camera_toolbar.addAction(photo_action)
