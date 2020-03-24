22342d6c76573592aee5aded0e2ad37ae2db1368
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-24 14:45:21 +0530

    updates

--- main.py
+++ main.py
@@ -9,3 +9,22 @@ from PyQt5.QtMultimediaWidgets import *
 import os
 import sys
 import time
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.available_cameras = QCameraInfo.availableCameras()
+        if not self.available_cameras:
+            pass #quit
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+
+        self.save_path = ""
+
+        self.viewfinder = QCameraViewfinder()
+        self.viewfinder.show()
+        self.setCentralWidget(self.viewfinder)
